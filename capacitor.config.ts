c91be1e2315e58810df3bd7f2d9dfbed8f1e78ae
Posirca Alexandru <76157684+Alexposh@users.noreply.github.com>
2025-08-10 13:52:21 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Saude4all',
+  webDir: 'www'
+};
+
+export default config;
