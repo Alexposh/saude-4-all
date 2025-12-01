bde3bb9886115bf12eaffc42a8ac59c3f349fd72
Posirca Alexandru <76157684+Alexposh@users.noreply.github.com>
2025-12-01 19:16:44 +0000

    update on patient

--- src/app/models/appointment.ts
+++ src/app/models/appointment.ts
@@ -1,8 +1,8 @@
 export interface Appointment{
     id: string;
-    gender:string;
     doctorId: string;
     patientId: string;
     locationId: string;
     dateOfAppointment: string;
+    status:string;
 }
